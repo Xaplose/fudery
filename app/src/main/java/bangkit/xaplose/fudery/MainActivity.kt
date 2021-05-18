cffb390210b1c6af339439bd61b4d8e37d5a59c4
Adhytia <adit.14.12.99@gmail.com>
2021-05-18 20:40:05 +0700

    Init project

---
+++ app/src/main/java/bangkit/xaplose/fudery/MainActivity.kt
@@ -0,0 +1,11 @@
+package bangkit.xaplose.fudery
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
